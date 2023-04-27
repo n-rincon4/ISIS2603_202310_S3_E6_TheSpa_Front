472df3915e1515872d0e69f2d031d1905019f42d
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-04-27 16:37:48 -0500

    Creado servicio-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SedeComponent } from './sede/sede.component';
+import { TrabajadorComponent } from './trabajador/trabajador.component';
+import { PackComponent } from './pack/pack.component';
+import { TrabajadorComponent } from './trabajador/trabajador.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      SedeComponent,
+      TrabajadorComponent,
+      PackComponent,
+      TrabajadorComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
