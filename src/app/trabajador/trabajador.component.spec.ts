4b190a3e95f59c7cb51becc0f1a21ab8ea60218d
Manuela Pacheco Malagón <m.pachecom2@uniandes.edu.co>
2023-05-08 15:05:12 -0500

    Tests Done

--- src/app/trabajador/trabajador.component.spec.ts
+++ src/app/trabajador/trabajador.component.spec.ts
@@ -1,16 +1,21 @@
 /* tslint:disable:no-unused-variable */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { TrabajadorComponent } from './trabajador.component';
+import { TrabajadorDetail } from './trabajador-detail';
 
 describe('TrabajadorComponent', () => {
   let component: TrabajadorComponent;
   let fixture: ComponentFixture<TrabajadorComponent>;
+  let debug: DebugElement;
 
   beforeEach((() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [TrabajadorComponent]
     })
       .compileComponents();
@@ -19,10 +24,33 @@ describe('TrabajadorComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TrabajadorComponent);
     component = fixture.componentInstance;
+
+    let testTrabajador: Array<TrabajadorDetail> = [];
+
+    for (let i = 0; i < 3; i++) {
+      testTrabajador[i] = new TrabajadorDetail(
+        faker.datatype.number(),
+        faker.lorem.words(5),
+        faker.image.imageUrl(),
+        faker.datatype.number(),
+        faker.datatype.boolean(),
+        [],
+        []
+      );
+    }
+
+    component.trabajadores = testTrabajador;
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a list of trabajadores', () => {
+    expect(component.trabajadores.length).toEqual(3);
   });
 });
