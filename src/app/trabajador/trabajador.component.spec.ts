c1d9634273be6cc80804e37e1d6a5e30a08d0cdb
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-05-08 18:44:30 -0500

    Added extra tests

--- src/app/trabajador/trabajador.component.spec.ts
+++ src/app/trabajador/trabajador.component.spec.ts
@@ -7,6 +7,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { TrabajadorComponent } from './trabajador.component';
 import { TrabajadorDetail } from './trabajador-detail';
+import { Servicio } from '../servicio/servicio';
+import { Sede } from '../sede/sede';
 
 describe('TrabajadorComponent', () => {
   let component: TrabajadorComponent;
@@ -26,23 +28,48 @@ describe('TrabajadorComponent', () => {
     component = fixture.componentInstance;
 
     let testTrabajador: Array<TrabajadorDetail> = [];
+    let testServicios: Array<Servicio> = [];
+    let testSedes: Array<Sede> = [];
 
     for (let i = 0; i < 3; i++) {
-      testTrabajador[i] = new TrabajadorDetail(
+      testSedes[i] = new Sede(
         faker.datatype.number(),
-        faker.lorem.words(5),
-        faker.image.imageUrl(),
-        faker.datatype.number(),
-        faker.datatype.boolean(),
-        [],
-        []
+        faker.lorem.sentence(),
+        faker.image.imageUrl()
       );
     }
 
-    component.trabajadores = testTrabajador;
+    for (let i = 0; i < 2; i++) {
+      testServicios[i] = new Servicio(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.paragraph(),
+        faker.datatype.number(),
+        faker.image.imageUrl(),
+        faker.datatype.number(),
+        faker.lorem.paragraph(),
+        faker.datatype.boolean(),
+        faker.lorem.sentence()
+      );
 
-    fixture.detectChanges();
-    debug = fixture.debugElement;
+      for (let i = 0; i < 3; i++) {
+        testTrabajador[i] = new TrabajadorDetail(
+          faker.datatype.number(),
+          faker.lorem.words(5),
+          faker.image.imageUrl(),
+          faker.datatype.number(),
+          faker.datatype.boolean(),
+          testSedes,
+          testServicios
+
+        );
+      }
+
+      component.trabajadores = testTrabajador;
+
+      fixture.detectChanges();
+      debug = fixture.debugElement;
+    }
   });
 
 
@@ -53,4 +80,20 @@ describe('TrabajadorComponent', () => {
   it('should have a list of trabajadores', () => {
     expect(component.trabajadores.length).toEqual(3);
   });
+
+  it('a trabajador should be a TrabajadorDetail', () => {
+    expect(component.trabajadores[0] instanceof TrabajadorDetail).toBeTruthy();
+  }
+  );
+
+
+  it('should have a list of sedes', () => {
+    expect(component.trabajadores[0].sedes).toBeTruthy();
+  }
+  );
+
+  it('should have a servicios', () => {
+    expect(component.trabajadores[0].servicios).toBeTruthy();
+  }
+  );
 });
