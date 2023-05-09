652cfab74b844f8eecd75fe8d06b382084ed562d
n-rincon4 <n.rincon4@uniandes.edu.co>
2023-05-08 21:17:07 -0500

    Test del servicio corregido

--- src/app/articulo/articulo.service.spec.ts
+++ src/app/articulo/articulo.service.spec.ts
@@ -1,11 +1,12 @@
 /* tslint:disable:no-unused-variable */
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ArticuloService } from './articulo.service';
 
 describe('Service: Articulo', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [ArticuloService]
     });
   });
