f1fff6b3b41dca9cfb686a142108b1b045c0346a
Juan Coronel <98646851+JuanCoronel@users.noreply.github.com>
2023-05-14 17:14:35 -0500

    First sede changes for new page model

--- src/app/sede/sede-list/sede-list.component.ts
+++ src/app/sede/sede-list/sede-list.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2, OnInit } from '@angular/core';
 import { SedeDetail } from '../sede-detail';
 import { SedeService } from '../sede.service';
+
+
 @Component({
   selector: 'app-sede-list',
   templateUrl: './sede-list.component.html',
   styleUrls: ['./sede-list.component.css']
 })
 export class SedeListComponent implements OnInit {
+
+  isOpen: boolean[] = [];
 
   sedes: SedeDetail[] = [];
   selectedSede: SedeDetail = undefined as any;
@@ -15,11 +19,17 @@ export class SedeListComponent implements OnInit {
 
   constructor(private sedeService: SedeService) { }
 
+
+
   getSedes(): void {
     this.sedeService.getSedes().subscribe(sedes => {
       // Apply the filter based on the searchText property
       this.sedes = sedes.filter(sede => sede.nombre.toLowerCase().includes(this.searchText.toLowerCase()));
     });
+  }
+
+  onSearchInput(value: string) {
+    this.searchText = value;
   }
 
   search() {
@@ -35,8 +45,13 @@ export class SedeListComponent implements OnInit {
 
   ngOnInit() {
 
+    this.isOpen = new Array(2).fill(false);
     this.getSedes();
 
+  }
+
+  toggleAccordion(index: number) {
+    this.isOpen[index] = !this.isOpen[index]; // Cambiamos el valor del índice seleccionado
   }
 
 }
