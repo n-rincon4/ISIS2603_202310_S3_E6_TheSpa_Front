9464e41d0cbae72a5ab7a9f1e924e7245bcc282a
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-05-15 12:11:38 -0500

    Listado y filtrado de paquetes

--- src/app/pack/pack-list/pack-list.component.ts
+++ src/app/pack/pack-list/pack-list.component.ts
@@ -9,14 +9,32 @@ import { PackDetail } from '../pack-detail';
 })
 export class PackListComponent implements OnInit {
 
+  searchNombre: String = '';
+  searchSedeNombre: String = '';
+  searchMinServicios: number = 0;
+  searchMinDescuento: number = 0;
   packs: PackDetail[] = [];
+  isOpen: boolean[] = [];
   selectedPack: PackDetail = undefined as any;
   selected = false;
 
   constructor(private packService: PackService) { }
 
   getPacks(): void {
-    this.packService.getPacks().subscribe(packs => this.packs = packs);
+
+    if (this.searchMinServicios == null || this.searchMinServicios == undefined || this.searchMinServicios.toString().length == 0) {
+      this.searchMinServicios = 0;
+    }
+
+    if (this.searchMinDescuento == null || this.searchMinDescuento == undefined || this.searchMinDescuento.toString().length == 0) {
+      this.searchMinServicios = 0;
+    }
+    // Apply the filter based on the searchNombre property
+
+    this.packService.getPacks().subscribe(packs => {
+      this.packs = packs.filter(pack => pack.nombre.toLowerCase().includes(this.searchNombre.toLowerCase()) && pack.sede.nombre.toLowerCase().includes(this.searchSedeNombre.toLowerCase()) && pack.servicios.length >= this.searchMinServicios && pack.descuento >= this.searchMinDescuento);
+    }
+    );
   }
 
   selectPack(pack: PackDetail): void {
@@ -26,6 +44,10 @@ export class PackListComponent implements OnInit {
 
   ngOnInit() {
     this.getPacks();
+  }
+
+  toggleAccordion(index: number) {
+    this.isOpen[index] = !this.isOpen[index]; // Cambiamos el valor del índice seleccionado
   }
 
 }
--- src/app/pack/pack.module.ts
+++ src/app/pack/pack.module.ts
@@ -4,12 +4,14 @@ import { PackDetailComponent } from './pack-detail/pack-detail.component';
 import { PackListComponent } from './pack-list/pack-list.component';
 import { RouterModule } from '@angular/router';
 import { PackRoutingModule } from './pack-routing.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    PackRoutingModule
+    PackRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [PackListComponent, PackDetailComponent],
   exports: [PackListComponent, PackDetailComponent]
