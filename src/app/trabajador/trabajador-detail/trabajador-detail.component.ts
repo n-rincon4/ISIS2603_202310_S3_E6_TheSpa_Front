e9bd42fc5deac6aa451056d4e30ce519824b8e0f
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-06-03 22:52:49 -0500

    Ahora hall of fame solo contiene el hall of fame

--- src/app/trabajador/trabajador-detail/trabajador-detail.component.ts
+++ src/app/trabajador/trabajador-detail/trabajador-detail.component.ts
@@ -12,6 +12,8 @@ export class TrabajadorDetailComponent implements OnInit {
 
   trabajadorId!: string;
   @Input() trabajadorDetail!: TrabajadorDetail;
+  rating: string = "";
+  enHallOfFame: string = "";
 
   constructor(private route: ActivatedRoute, private trabajadorService: TrabajadorService) { }
 
@@ -19,6 +21,19 @@ export class TrabajadorDetailComponent implements OnInit {
     let id: number = +this.trabajadorId;
     this.trabajadorService.getTrabajador(id).subscribe(trabajador => {
       this.trabajadorDetail = <TrabajadorDetail>trabajador;
+      // for each rating, add a star to the rating string
+      for (let i = 0; i < this.trabajadorDetail.calificacion; i++) {
+        // a star is the string '★' (not the character '✩')
+        this.rating += '★';
+      }
+      // if enHallOfFame is true, the string enHallOfFame is 'Sí'
+      if (this.trabajadorDetail.enHallOfFame) {
+        this.enHallOfFame = 'Sí';
+      }
+      // if enHallOfFame is false, the string enHallOfFame is 'No'
+      else {
+        this.enHallOfFame = 'No';
+      }
     })
   }
 
